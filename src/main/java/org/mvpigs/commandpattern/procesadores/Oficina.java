de2b94c911d32467c45111004372fb0fe3d0f377
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-06-01 13:02:12 +0200

    Added method printarStatus, depends if tractable or not will return accepted or rejected

--- src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
+++ src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
@@ -1,5 +1,6 @@
 package org.mvpigs.commandpattern.procesadores;
 
+import org.mvpigs.commandpattern.interfaces.Pedido;
 import org.mvpigs.commandpattern.interfaces.Procesador;
 import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
 
@@ -11,5 +12,12 @@ public class Oficina implements Procesador {
             return true;
         }
         return false;
+    }
+
+    public String printarStatus(Boolean tratable, Pedido pedido){
+        if (tratable){
+            return pedido.destino() + " " + Status.ACEPTADO.getEstado();
+        }
+        return pedido.destino() + " " + Status.RECHAZADO.getEstado();
     }
 }
