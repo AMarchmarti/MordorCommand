e6b31f5a82742c487a30ee0b7fe6e0d29a81b048
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-06-01 13:10:34 +0200

    added a constructor with a list of Pedido

--- src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
+++ src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
@@ -1,4 +1,15 @@
 package org.mvpigs.commandpattern.tratamientos;
 
+import org.mvpigs.commandpattern.interfaces.Pedido;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TratamientoPedidoMultiple {
+
+    private List<Pedido> listaPedidos = new ArrayList<>();
+
+    public TratamientoPedidoMultiple(List<Pedido> pedidos){
+        this.listaPedidos = pedidos;
+    }
 }
