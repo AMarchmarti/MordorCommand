0fbc4afcea9cbe61b1493d67be05a3909983a7aa
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-06-01 13:39:33 +0200

    implement the interface TratamientoPedido and the methods for calculate the total weight and the total lumps

--- src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
+++ src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
@@ -1,15 +1,45 @@
 package org.mvpigs.commandpattern.tratamientos;
 
 import org.mvpigs.commandpattern.interfaces.Pedido;
+import org.mvpigs.commandpattern.interfaces.PedidoPeligroso;
+import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-public class TratamientoPedidoMultiple {
+public class TratamientoPedidoMultiple implements TratamientoPedido {
 
-    private List<Pedido> listaPedidos = new ArrayList<>();
+    private Set<Pedido> listaPedidos = new HashSet<>();
 
-    public TratamientoPedidoMultiple(List<Pedido> pedidos){
+    private Integer numBultos = 0;
+
+    private Integer pesoTotal = 0;
+
+    private Pedido pedido = null;
+
+    private PedidoPeligroso pedidoPeligroso = null;
+
+    public TratamientoPedidoMultiple(Set<Pedido> pedidos){
         this.listaPedidos = pedidos;
     }
+
+    @Override
+    public boolean tratar() {
+        if (listaPedidos.size() == this.getNumBultos() && pesoTotal > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public void calcularTotalBultos(){
+        this.numBultos = this.listaPedidos.size();
+    }
+
+    public Integer getNumBultos(){return this.numBultos;}
+
+    public void calcularPesoTotal(){
+        this.pesoTotal = listaPedidos.stream().map(Pedido::peso).reduce(0,Integer::sum);
+    }
+
+    public Integer getPesoTotal(){return this.pesoTotal;}
 }
